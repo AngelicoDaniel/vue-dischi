6c5ae5177c12c0dd822ffba1ec58f4c8e0cd0356
Daniel <angelicodaniel93@gmail.com>
2022-11-25 14:11:49 +0100

    HeaderComp

---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div>
+      <img src="../assets/logo.png" alt="">
+    </div>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComp',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+div{
+  height: 80px;
+  background-color: #2E3A46 ;
+  text-align: start;
+  img{
+    width: 50px;
+    padding-top: 15px;
+    margin-left: 20px;
+  }
+}
+</style>
